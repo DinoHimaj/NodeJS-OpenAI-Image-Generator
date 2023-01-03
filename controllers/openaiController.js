8b300bb4d2704850108e1e5b0a063f4e74ae130a
Dino Himaj <dinohimaj@rocketmail.com>
2023-01-03 22:27:42 +0100

    finished backend

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,41 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateImage = async (req, res) => {
-  res.status(200).json({
-    succes: true,
-  });
+  const { prompt, size } = req.body;
+
+  const imageSize =
+    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: imageSize,
+    });
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    //This is error feedback for developers (view it in browser console)
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+    //This is what user sees in frontend
+    res.status(400).json({
+      success: false,
+      data: 'This image could not be generated',
+    });
+  }
 };
 
 module.exports = { generateImage };
